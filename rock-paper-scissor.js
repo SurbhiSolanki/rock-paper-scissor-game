7da716cdd4815c183fbeb9486634b8fcda682754
SurbhiSolanki <surbhi1543@gmail.com>
2023-11-02 16:25:36 +0530

    Added new project

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -100,8 +100,8 @@ function playGame(playerMove){
   
   document.querySelector('.js-result').innerHTML = result;
   document.querySelector('.js-moves').innerHTML = ` You
-  <img src="images/${playerMove}-emoji.png" class="move-icon">
-  <img src="images/${computerMove}-emoji.png" class="move-icon">
+  <img src="${playerMove}-emoji.png" class="move-icon">
+  <img src="${computerMove}-emoji.png" class="move-icon">
   Computer
 `;
 //           alert(`you picked ${playerMove}.computer picked ${computerMove}. ${result}
